56589a3892187ae1da964251353ba7f7781b3fe7
Warda Ayaz <wayaz107@gmail.com>
2020-09-04 06:06:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT bears.name and bears.age WHERE gender = 'F' "
+  "SELECT bears.name, bears.age WHERE gender = 'F' "
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
